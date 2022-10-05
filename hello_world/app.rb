567139c8878c4523f7f830c1c46f8a1a6a40dc06
jeremira <jmiraille@gmail.com>
2022-10-05 08:30:00 +0200

    whatever

--- hello_world/app.rb
+++ hello_world/app.rb
@@ -1,6 +1,7 @@
 require 'httparty'
 require 'json'
 require 'aws-sdk-s3'
+#require "mini_magick"
 require "image_processing/mini_magick"
 
 def lambda_handler(event:, context:)
@@ -10,13 +11,19 @@ def lambda_handler(event:, context:)
     object = s3.bucket('kosube').object(object_name)
     original_file = "/tmp/#{object_name}"
     thumbnail_file = "/tmp/thu_#{object_name}"
-    low_res_file = "/tmp/low_#{object_name}"
+    # low_res_file = "/tmp/low_#{object_name}"
     object.get response_target: original_file
 
-  #   ImageProcessing::Vips.source(original_file).convert("jpg").saver(quality: 100).resize_to_fill(100, 100, crop: :centre).call(destination: thumbnail_file)
+    # image = MiniMagick::Image.open original_file
+    # image.resize "100x100"
+    # image.format "jpg"
+    # image.quality 100
+    # image.write thumbnail_file
+    # ImageProcessing::MiniMagick.source(original_file).convert("jpg").saver(quality: 100).resize_to_fill(100, 100, gravity: 'Center').call(destination: thumbnail_file)
+    ImageProcessing::Vips.source(original_file).convert("jpg").saver(quality: 100).resize_to_fill(100, 100, crop: :center).call(destination: thumbnail_file)
   #   ImageProcessing::Vips.source(original_file).convert("jpg").saver(quality: 95).resize_to_limit(1200, 1200).call(destination: low_res_file)
   #
-  #   s3.bucket('kosube-thumbnail').object("thu_#{object_name}").upload_file(thumbnail_file)
+    s3.bucket('kosube-thumbnail').object("thu_#{object_name}").upload_file(thumbnail_file)
   #   s3.bucket('kosube-low').object("low_#{object_name}").upload_file(low_res_file)
 
   {
@@ -24,9 +31,10 @@ def lambda_handler(event:, context:)
     body: {object_name: object_name}.to_json
   }
 end
-
-# def lambda_layer
-#   LAYER_NAME="my-ruby-layer"
+# https://github.com/stechstudio/libvips-lambda
+# script ton zip a layer
+# ==============================
+#   LAYER_NAME="layer-kosube-gems"
 #   mkdir $LAYER_NAME && cd $_
 #
 #   bundle init
@@ -36,17 +44,16 @@ end
 #   bundle add image_processing --skip-install
 #   rm Gemfile.lock
 #
-#   docker run --rm -v $PWD:/var/layer \
-#              -w /var/layer \
-#              amazon/aws-sam-cli-build-image-ruby2.7 \
-#              bundle install --path=ruby
+# docker run --rm -v $PWD:/var/layer \
+#            -w /var/layer \
+#            amazon/aws-sam-cli-build-image-ruby2.7 \
+#            bundle install --path=ruby
 #
-#   mv ruby/ruby ruby/gems
-#   zip -r layer.zip ruby
-#
-#   aws lambda publish-layer-version \
-#              --layer-name $LAYER_NAME \
-#              --region eu-west-1 \
-#              --compatible-runtimes ruby2.7 \
-#              --zip-file fileb://layer.zip
-# end
+# sudo mv ruby/ruby ruby/gems
+# zip -r layer.zip ruby
+# #
+# aws lambda publish-layer-version \
+#            --layer-name $LAYER_NAME \
+#            --region eu-west-1 \
+#            --compatible-runtimes ruby2.7 \
+#            --zip-file fileb://layer.zip
